fc15a1858982bc28668fca320da1b6374fd24392
Andrew Kelley <superjoe30@gmail.com>
2013-07-13 15:03:03 -0400

    template: switch spacing from 4 spaces to 2

--- templates/readme/src/main.js
+++ templates/readme/src/main.js
@@ -2,61 +2,60 @@ var chem = require("chem");
 var Vec2d = chem.vec2d;
 
 chem.onReady(function () {
-    var canvas = document.getElementById("game");
-    var engine = new chem.Engine(canvas);
-    var batch = new chem.Batch();
-    var boom = new chem.Sound('sfx/boom.ogg');
-    var ship = new chem.Sprite('ship', {
-        batch: batch,
-        pos: new Vec2d(200, 200),
-        rotation: Math.PI / 2
-    });
-    var shipVel = new Vec2d();
-    var rotationSpeed = Math.PI * 0.04;
-    var thrustAmt = 0.1;
-    engine.on('update', function (dt, dx) {
-        ship.pos.add(shipVel);
+  var canvas = document.getElementById("game");
+  var engine = new chem.Engine(canvas);
+  var batch = new chem.Batch();
+  var boom = new chem.Sound('sfx/boom.ogg');
+  var ship = new chem.Sprite('ship', {
+    batch: batch,
+    pos: new Vec2d(200, 200),
+    rotation: Math.PI / 2
+  });
+  var shipVel = new Vec2d();
+  var rotationSpeed = Math.PI * 0.04;
+  var thrustAmt = 0.1;
+  engine.on('update', function (dt, dx) {
+    ship.pos.add(shipVel);
 
-        // rotate the ship with left and right arrow keys
-        if (engine.buttonState(chem.button.KeyLeft)) {
-            ship.rotation -= rotationSpeed * dx;
-        }
-        if (engine.buttonState(chem.button.KeyRight)) {
-            ship.rotation += rotationSpeed * dx;
-        }
+    // rotate the ship with left and right arrow keys
+    if (engine.buttonState(chem.button.KeyLeft)) {
+      ship.rotation -= rotationSpeed * dx;
+    }
+    if (engine.buttonState(chem.button.KeyRight)) {
+      ship.rotation += rotationSpeed * dx;
+    }
 
-        // apply forward and backward thrust with up and down arrow keys
-        var thrust = new Vec2d(Math.cos(ship.rotation), Math.sin(ship.rotation));
-        if (engine.buttonState(chem.button.KeyUp)) {
-            shipVel.add(thrust.scaled(thrustAmt * dx));
-        }
-        if (engine.buttonState(chem.button.KeyDown)) {
-            shipVel.sub(thrust.scaled(thrustAmt * dx));
-        }
+    // apply forward and backward thrust with up and down arrow keys
+    var thrust = new Vec2d(Math.cos(ship.rotation), Math.sin(ship.rotation));
+    if (engine.buttonState(chem.button.KeyUp)) {
+      shipVel.add(thrust.scaled(thrustAmt * dx));
+    }
+    if (engine.buttonState(chem.button.KeyDown)) {
+      shipVel.sub(thrust.scaled(thrustAmt * dx));
+    }
 
-        // press space to blow yourself up
-        if (engine.buttonJustPressed(chem.button.KeySpace)) {
-            boom.play();
-            ship.setAnimationName('boom');
-            ship.setFrameIndex(0);
-            ship.on('animationend', function() {
-                ship.delete();
-            });
-        }
-    });
-    engine.on('draw', function (context) {
-        // clear canvas to black
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, engine.size.x, engine.size.y);
+    // press space to blow yourself up
+    if (engine.buttonJustPressed(chem.button.KeySpace)) {
+      boom.play();
+      ship.setAnimationName('boom');
+      ship.setFrameIndex(0);
+      ship.on('animationend', function() {
+        ship.delete();
+      });
+    }
+  });
+  engine.on('draw', function (context) {
+    // clear canvas to black
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, engine.size.x, engine.size.y);
 
-        // draw all sprites in batch
-        engine.draw(batch);
+    // draw all sprites in batch
+    engine.draw(batch);
 
-        // draw a little fps counter in the corner
-        context.fillStyle = '#ffffff'
-        engine.drawFps();
-    });
-    engine.start();
-    canvas.focus();
+    // draw a little fps counter in the corner
+    context.fillStyle = '#ffffff'
+    engine.drawFps();
+  });
+  engine.start();
+  canvas.focus();
 });
-
